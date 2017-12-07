ec0e2af038892ed826f6e39db1cf327af260622c
fraser1234 <fraseralexanderblack@gmail.com>
2017-12-07 14:09:18 +0000

    employee getters pass

---
+++ src/main/java/Staff/Employee.java
@@ -0,0 +1,27 @@
+package Staff;
+
+public class Employee {
+
+    private String name;
+    private String nationalNumber;
+    private int salary;
+
+    public Employee(String name, String nationalNumber, int salary){
+        this.name = name;
+        this.nationalNumber = nationalNumber;
+        this.salary = salary;
+    }
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNatNumber() {
+        return nationalNumber;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+}
---
+++ src/test/java/EmployeeTest.java
@@ -0,0 +1,33 @@
+import Staff.Employee;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class EmployeeTest {
+
+    Employee employee;
+
+    @Before
+    public void before(){
+        employee = new Employee("Alex", "2dd33", 25000);
+    }
+
+
+    @Test
+    public void getName(){
+        assertEquals("Alex", employee.getName());
+    }
+
+    @Test
+    public void getNatNumber(){
+        assertEquals("2dd33", employee.getNatNumber());
+    }
+
+    @Test
+    public void getSalary(){
+        assertEquals(25000, employee.getSalary());
+    }
+
+
+}
