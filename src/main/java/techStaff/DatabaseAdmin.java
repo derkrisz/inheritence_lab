989cddcec8ab0e76dee73b758313b98e8d83a88b
fraser1234 <fraseralexanderblack@gmail.com>
2017-12-07 14:40:25 +0000

    developer and dbadmin subclasses created

---
+++ src/main/java/techStaff/DatabaseAdmin.java
@@ -0,0 +1,9 @@
+package techStaff;
+
+import Staff.Employee;
+
+public class DatabaseAdmin extends Employee{
+    public DatabaseAdmin(String name, String nationalNumber, int salary) {
+        super(name, nationalNumber, salary);
+    }
+}
---
+++ src/main/java/techStaff/Developer.java
@@ -0,0 +1,9 @@
+package techStaff;
+
+import Staff.Employee;
+
+public class Developer extends Employee {
+    public Developer(String name, String nationalNumber, int salary) {
+        super(name, nationalNumber, salary);
+    }
+}
